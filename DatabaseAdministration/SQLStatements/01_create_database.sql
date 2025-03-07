d9d50bfa9084c7f1c9e794fd76adfe3809f3b8f6
Nathalia Sungkoora <nathalia.sungkoora@sambeconsulting.com>
2025-03-08 01:56:00 +0200

    Adding SQL Query to create a database for SQL Server Automation Project

---
+++ DatabaseAdministration/SQLStatements/01_create_database.sql
@@ -0,0 +1,7 @@
+
+-- Check if the 'AutoTest' database exists; if not, create it.
+IF NOT EXISTS (SELECT name FROM sys.databases where name = 'AutoTest')
+BEGIN
+	CREATE DATABASE AutoTest;	-- Create the 'AutoTest' database
+END;
+GO

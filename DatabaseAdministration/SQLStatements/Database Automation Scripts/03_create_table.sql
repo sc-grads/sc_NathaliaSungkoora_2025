c65cec5a22200ffd5fb91ae74bd7a060375493e6
Nathalia Sungkoora <nathalia.sungkoora@sambeconsulting.com>
2025-03-26 22:47:31 +0200

    Adding 03_create_table.sql script for Automation Project Part 2

---
+++ DatabaseAdministration/SQLStatements/Database Automation Scripts/03_create_table.sql
@@ -0,0 +1,18 @@
+
+USE AutoDBNathalia;
+GO
+
+IF NOT EXISTS (
+    SELECT * FROM INFORMATION_SCHEMA.TABLES 
+    WHERE TABLE_NAME = 'users' AND TABLE_TYPE = 'BASE TABLE'
+)
+BEGIN
+    CREATE TABLE users (
+        ID INT IDENTITY(1,1) PRIMARY KEY,
+        Name NVARCHAR(100),
+        Surname NVARCHAR(100),
+        Email NVARCHAR(100),
+        Created DATETIME DEFAULT GETDATE()
+    );
+END;
+GO

bc31126cb31ab4d031ac9a428a904790a703a11b
Nathalia Sungkoora <nathalia.sungkoora@sambeconsulting.com>
2025-03-26 10:45:23 +0200

    Pushing script to trigger workflow test again

--- DatabaseAdministration/SQLStatements/Database Automation Scripts/01_create_database.sql
+++ DatabaseAdministration/SQLStatements/Database Automation Scripts/01_create_database.sql
@@ -1,7 +1,8 @@
-   
+
 -- Check if the 'AutoTest' database exists; if not, create it.
 IF NOT EXISTS (SELECT name FROM sys.databases where name = 'AutoTest')
 BEGIN
 	CREATE DATABASE AutoTest;	-- Create the 'AutoTest' database
 END;
 GO
+ 

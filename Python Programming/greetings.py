0a536e649dd5a534efbcda8ef6286e80f765b948
Nathalia Sungkoora <nathalia.sungkoora@sambeconsulting.com>
2025-05-07 15:26:30 +0200

    Adding Udemy Python Section 6 files

---
+++ Python Programming/greetings.py
@@ -0,0 +1,12 @@
+
+from typing import Final
+
+# Constants
+AUTHOR: Final[str] = 'Nathalia'
+VERSION: Final[int] = 1
+
+# Function definition
+def greet(name: str) -> None:
+    print(f'Hello, {name}!')
+
+

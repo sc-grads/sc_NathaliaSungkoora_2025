aeeec1843c2405db90c2624da4c787bce5d8697d
Nathalia Sungkoora <nathalia.sungkoora@sambeconsulting.com>
2025-06-04 09:58:06 +0200

    Adding Modified DB and Table SQL Scripts for Automated Timesheet Project.

--- Hands-on Projects/Timesheet/SQL Scripts/CreateDbAndTables.sql
+++ Hands-on Projects/Timesheet/SQL Scripts/CreateDbAndTables.sql
@@ -1,3 +1,5 @@
+/* SQL STATEMENTS FOR AUTOMATED SSIS TIMESHEET PROJECT*/
+
 -- Create the database
 CREATE DATABASE TimesheetDB;
 GO
@@ -5,7 +7,8 @@ GO
 Use TimesheetDB
 Go
 
-  -- Drop existing table if it exists
+ --EMPLOYEE TABLE: 
+ --Drop existing table if it exists
 IF OBJECT_ID('dbo.Employee', 'U') IS NOT NULL
 BEGIN
     DROP TABLE dbo.Employee;
@@ -14,7 +17,8 @@ GO
 
 CREATE TABLE Employee(FullName VARCHAR(100) PRIMARY KEY);
 
-  --TIMESHEET: Drop existing table if it exists
+  --TIMESHEET TABLE: 
+  --Drop existing table if it exists
 IF OBJECT_ID('dbo.Timesheet', 'U') IS NOT NULL
 BEGIN
     DROP TABLE dbo.Timesheet;
@@ -42,8 +46,92 @@ CREATE TABLE dbo.Timesheet (
 
 
 
+--CLIENT TABLE: 
 
---INSERTING Employees into Table
+-- Drop Client table if it exists
+IF OBJECT_ID('dbo.Client', 'U') IS NOT NULL
+BEGIN
+    DROP TABLE dbo.Client;
+END
+GO
+
+-- Create Client table
+CREATE TABLE dbo.Client (
+    ClientID INT IDENTITY(1,1) PRIMARY KEY,
+    ClientName VARCHAR(255) NOT NULL UNIQUE
+);
+GO
+
+--PROJECT TABLE:
+-- Drop Project table if it exists
+IF OBJECT_ID('dbo.Project', 'U') IS NOT NULL
+BEGIN
+    DROP TABLE dbo.Project;
+END
+GO
+
+-- Create Project table
+CREATE TABLE dbo.Project (
+    ProjectID INT IDENTITY(1,1) PRIMARY KEY,
+    ClientID INT NOT NULL,
+    ProjectName VARCHAR(255) NOT NULL,
+    FOREIGN KEY (ClientID) REFERENCES dbo.Client(ClientID)
+);
+GO
+
+--LEAVE TABLE:
+-- Drop Leave table if it exists
+IF OBJECT_ID('dbo.Leave', 'U') IS NOT NULL
+BEGIN
+    DROP TABLE dbo.Leave;
+END
+GO
+
+-- Create Leave table
+CREATE TABLE dbo.Leave (
+    LeaveID INT IDENTITY(1,1) PRIMARY KEY,
+    EmployeeID INT NOT NULL, -- FK to Employee table
+    LeaveType VARCHAR(100) NOT NULL,
+    StartDate DATE NOT NULL,
+    EndDate DATE NOT NULL,
+    NumberOfDays DECIMAL(4,1) NOT NULL,
+    ApprovalObtained BIT, -- 1 for Yes, 0 for No
+    SickNoteProvided BIT, -- 1 for Yes, 0 for No
+    ContactInfoDuringLeave VARCHAR(255),
+    SubmissionDate DATE,
+    EmployeeSignature VARCHAR(100), 
+    ClientSignature VARCHAR(100), 
+    --FOREIGN KEY (EmployeeID) REFERENCES dbo.Employee(EmployeeID)
+);
+GO
+
+--EXPENSE TABLE
+-- Drop Expense table if it exists
+IF OBJECT_ID('dbo.Expense', 'U') IS NOT NULL
+BEGIN
+    DROP TABLE dbo.Expense;
+END
+GO
+
+-- Create Expense table
+CREATE TABLE dbo.Expense (
+    ExpenseID INT IDENTITY(1,1) PRIMARY KEY,
+    EmployeeID INT NOT NULL, -- link to Employee table
+    ExpenseMonth VARCHAR(20),
+    ExpenseDescription VARCHAR(255),
+    ExpenseType VARCHAR(100),
+    Cost DECIMAL(10,2), -- don't use "R", use numbers only
+    SubmissionDate DATE DEFAULT GETDATE(),
+    --FOREIGN KEY (EmployeeID) REFERENCES dbo.Employee(EmployeeID)
+);
+GO
+
+
+
+
+--------------------------------------------------------------------------------------
+
+--INSERTING Employees into Employee Table:
 INSERT INTO [dbo].[Employee](FullName)
 VALUES
   ('Evashan Govender'),
@@ -53,5 +141,11 @@ VALUES
   ('Neo Motaung'),
   ('Siphenathi Ndevu');
 
+
+  --SELECT Statements:
   SELECT TOP (1000) [FullName]
   FROM [TimesheetDB].[dbo].[Employee]
+
+
+
+ 

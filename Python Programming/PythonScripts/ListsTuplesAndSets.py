542c580d74f0d45014b5f0a1c3fab409fd81ce24
Nathalia Sungkoora <nathalia.sungkoora@sambeconsulting.com>
2025-06-23 11:47:53 +0200

    Modified Lists Tuples and Sets to add Python  Exercise Script for Udemy course.

--- Python Programming/PythonScripts/ListsTuplesAndSets.py
+++ Python Programming/PythonScripts/ListsTuplesAndSets.py
@@ -40,3 +40,20 @@ print(s) #Output= {'Smith', 'Rolf', 'Anne', 'Bob'}
 
 s.add("Smith")
 print(s) #Same Output (no duplicates in set)= {'Smith', 'Rolf', 'Anne', 'Bob'}
+
+
+
+#Lists, tuples, and set (Python 3.10)
+#EXERCISE
+
+# Create a list, called my_list, with three numbers. The total of the numbers added together should be 100.
+my_list=[60, 30, 10]
+
+# Create a tuple, called my_tuple, with a single value in it
+my_tuple = (10,)
+
+# Modify set2 so that set1.intersection(set2) returns {5, 77, 9, 12}
+set1 = {14, 5, 9, 31, 12, 77, 67, 8}
+set2 = {5, 77, 9, 12}
+both = set1.intersection(set2)
+print(both)

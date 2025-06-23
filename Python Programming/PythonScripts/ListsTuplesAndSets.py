76aaf5cf277cfdfa7b1646adbe7ff471f2ad7308
Nathalia Sungkoora <nathalia.sungkoora@sambeconsulting.com>
2025-06-23 10:41:29 +0200

    Adding First Python App and Lists, Tuples and Sets Python Scripts for Udemy course

---
+++ Python Programming/PythonScripts/ListsTuplesAndSets.py
@@ -0,0 +1,42 @@
+# LISTS, TUPLES AND SETS
+
+
+l = ["Bob", "Rolf", "Anne"] #Lists: Defined by square brackets and seperated by a comma. Order is kept.
+t = ("Bob", "Rolf", "Anne") #Tuple:  Defined by curved brackets and seperated by a comma. Order is kept.
+s = {"Bob", "Rolf", "Anne"} # Set:Defined by curly brackets seperated by a comma. Can't have duplicate elements. Order not guaranteed, can change at any moment.
+
+#Use index to access individual items in lists and tuples (order stays the same):
+#Cannot do the same for sets as order is not gauranteed and can change
+
+print(l[0]) #Bob
+print(t[0]) #Bob
+# print(s[0])  Gives error
+
+# Modify individual items in lists using the index:
+
+l[0] = "Smith" #List changes to ['Smith', 'Rolf', 'Anne']
+# t[0] = "Smith"  # Gives an error because tuples are "immutable"(Cannot be modified after they have been created).
+
+print(l)
+print(t)
+
+# Add elements to a list by using `.append`
+
+l.append("Jen")
+print(l) #Output= ['Smith', 'Rolf', 'Anne', 'Jen']
+# Tuples cannot be appended to because they are immutable (Cannot be modified after they have been created).
+
+
+# Remove elements from list 
+l.remove("Smith")
+print(l[1]) # Output: ['Rolf', 'Anne', 'Jen']
+
+# Add to sets by using `.add`
+
+s.add("Smith")
+print(s) #Output= {'Smith', 'Rolf', 'Anne', 'Bob'}
+
+# Sets can't have the same element twice.
+
+s.add("Smith")
+print(s) #Same Output (no duplicates in set)= {'Smith', 'Rolf', 'Anne', 'Bob'}

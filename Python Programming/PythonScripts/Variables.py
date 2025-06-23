e4d1e6b0932e5b736bc79aef49b72404211df100
Nathalia Sungkoora <nathalia.sungkoora@sambeconsulting.com>
2025-06-23 10:12:36 +0200

    Adding new Python Scripts for Udemy course

---
+++ Python Programming/PythonScripts/Variables.py
@@ -0,0 +1,53 @@
+#VARIABLES in Python
+
+# %%
+a = 4
+b = 3.998
+print(a * b)
+# multiply two numbers
+
+x = 15
+price = 9.99
+
+discount = 0.2
+
+result = price * (1 - discount)
+
+print(result) #7.992000000000001
+# calculate discounted price
+
+#STRINGS
+
+name = "Rolf"
+print(name) #Rolf
+# print the name
+
+name = "Rolf"
+print(name * 2) #RolfRolf (name+name)
+# repeat the string twice
+
+a = 25
+b = a 
+print(a) #25
+print(b) #25
+
+b=17
+
+print(a) #25
+print(b) #17
+
+
+#VARIABLES EXERCISE
+
+#Variables (Python 3.10)
+# Create two variables, var1 and var2, both with the same value.
+var1 = "Hello"
+var2= "Hello"
+print(var1)
+print(var2)
+
+# Create two variables, num1 and num2, which multiply together to give 16.
+num1 = 4
+num2 = 4
+
+print (num1*num2)

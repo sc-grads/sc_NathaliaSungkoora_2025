65a06f213f6068ec5d5f56cd3edb3bd8bcabc6dc
Nathalia Sungkoora <nathalia.sungkoora@sambeconsulting.com>
2025-09-09 14:50:51 +0200

    Adding Web Server files for PostgreSQL Udemy course.

---
+++ DatabaseAdministration/SQLStatements/PostgreSQL/WebServer/ig/migrations/1757421441251_drop-lng-and-lat-from-posts.js
@@ -0,0 +1,29 @@
+/**
+ * @type {import('node-pg-migrate').ColumnDefinitions | undefined}
+ */
+export const shorthands = undefined;
+
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const up = (pgm) => {
+    pgm.sql (`
+        ALTER TABLE posts
+        DROP COLUMN lat,
+        DROP COLUMN lng;
+        `);
+};
+/**
+ * @param pgm {import('node-pg-migrate').MigrationBuilder}
+ * @param run {() => void | undefined}
+ * @returns {Promise<void> | void}
+ */
+export const down = (pgm) => {
+    pgm.sql (`
+        ALTER TABLE posts
+        ADD COLUMN lat NUMERIC,
+        ADD COLUMN lng NUMERIC;
+        `);
+};
